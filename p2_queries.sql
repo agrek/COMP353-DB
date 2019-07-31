105b25514d2f583ab7020b9bdad5c64be745c2c8
agrek <agrek@users.noreply.github.com>
2019-07-30 23:32:09 -0400

    Formatted whitespace

--- p2_queries.sql
+++ p2_queries.sql
@@ -7,10 +7,10 @@ FROM Campus;
 /*	V
 	Give a list of all buildings on a given campus.
 */
-SELECT CONCAT(C.name, ' (', C.abbreviation, ')') AS Campus,
+SELECT CONCAT(C.name, ' (', C.abbreviation, ')')                          AS Campus,
        GROUP_CONCAT(B.name, ' (', B.abbreviation, ')' SEPARATOR ', <br>') AS Buildings,
-       COUNT(*) AS numOfBuildings
+       COUNT(*)                                                           AS numOfBuildings
 FROM Building B
-INNER JOIN Campus C on B.campus = C.abbreviation
+         INNER JOIN Campus C ON B.campus = C.abbreviation
 GROUP BY C.name;
 
