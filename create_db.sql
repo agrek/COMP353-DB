29f9ff70282ff005fa1c7ff7b1cf32174459808a
agrek <agrek@users.noreply.github.com>
2019-07-30 23:38:44 -0400

    Formatted whitespace

--- create_db.sql
+++ create_db.sql
@@ -405,8 +405,8 @@ CREATE TABLE ResearchFundingApplications
     status           ENUM ('Applied', 'Granted', 'Denied') NOT NULL,
     student_ssn      INT                                   NULL,
     research_fund_id INT                                   NOT NULL,
-    year             INT(4)                                   NOT NULL,
-    term             VARCHAR(45) NOT NULL,
+    year             INT(4)                                NOT NULL,
+    term             VARCHAR(45)                           NOT NULL,
     CONSTRAINT ResearchFundingApplications_pk
         PRIMARY KEY (id),
     CONSTRAINT ResearchFundingApplications_GradStudents_ssn_fk
@@ -857,56 +857,56 @@ BEGIN
                                                  INNER JOIN separatedNew ON separatedNew.id = newRow.id);
 
     /******************* Room Time Conflict Check *******************/
-        /* Fetching all sections taught in same building, room_number, year, and term*/
-        CREATE TEMPORARY TABLE oldRoomSec AS (SELECT Section.id, day, start_time, end_time, term, year, room_number
-                                              FROM Section
-                                              WHERE building_abbreviation = NEW.building_abbreviation
-                                                AND room_number = NEW.room_number
-                                                AND year = NEW.year
-                                                AND term = NEW.term);
+    /* Fetching all sections taught in same building, room_number, year, and term*/
+    CREATE TEMPORARY TABLE oldRoomSec AS (SELECT Section.id, day, start_time, end_time, term, year, room_number
+                                          FROM Section
+                                          WHERE building_abbreviation = NEW.building_abbreviation
+                                            AND room_number = NEW.room_number
+                                            AND year = NEW.year
+                                            AND term = NEW.term);
 
-        CREATE TEMPORARY TABLE separatedOld AS (SELECT oldRoomSec.id,
-                                                       SUBSTRING_INDEX(SUBSTRING_INDEX(oldRoomSec.day, ', ', numbers.n),
-                                                                       ', ', -1) day
-                                                FROM numbers
-                                                         INNER JOIN oldRoomSec
-                                                                    ON CHAR_LENGTH(oldRoomSec.day)
-                                                                           -
-                                                                       CHAR_LENGTH(REPLACE(oldRoomSec.day, ', ', '')) >=
-                                                                       numbers.n - 1
-                                                ORDER BY id, n);
+    CREATE TEMPORARY TABLE separatedOld AS (SELECT oldRoomSec.id,
+                                                   SUBSTRING_INDEX(SUBSTRING_INDEX(oldRoomSec.day, ', ', numbers.n),
+                                                                   ', ', -1) day
+                                            FROM numbers
+                                                     INNER JOIN oldRoomSec
+                                                                ON CHAR_LENGTH(oldRoomSec.day)
+                                                                       -
+                                                                   CHAR_LENGTH(REPLACE(oldRoomSec.day, ', ', '')) >=
+                                                                   numbers.n - 1
+                                            ORDER BY id, n);
 
-        CREATE TEMPORARY TABLE oldSecs AS (SELECT Section.id,
-                                                  separatedOld.day,
-                                                  start_time,
-                                                  end_time,
-                                                  term,
-                                                  year,
-                                                  building_abbreviation,
-                                                  room_number
-                                           FROM Section
-                                                    INNER JOIN separatedOld ON separatedOld.id = Section.id);
+    CREATE TEMPORARY TABLE oldSecs AS (SELECT Section.id,
+                                              separatedOld.day,
+                                              start_time,
+                                              end_time,
+                                              term,
+                                              year,
+                                              building_abbreviation,
+                                              room_number
+                                       FROM Section
+                                                INNER JOIN separatedOld ON separatedOld.id = Section.id);
 
-        CREATE TEMPORARY TABLE conflictSecs AS (SELECT oldSecs.day         d1,
-                                                       newEntry.day        d2,
-                                                       oldSecs.start_time  s1,
-                                                       newEntry.start_time s2,
-                                                       oldSecs.end_time    e1,
-                                                       newEntry.end_time   e2
-                                                FROM oldSecs
-                                                         INNER JOIN newEntry ON oldSecs.day = newEntry.day
-                                                WHERE ((oldSecs.start_time >= newEntry.start_time) AND
-                                                       (oldSecs.start_time < newEntry.end_time))
-                                                   OR ((newEntry.start_time >= oldSecs.start_time) AND
-                                                       (newEntry.start_time < oldSecs.end_time))
-        );
+    CREATE TEMPORARY TABLE conflictSecs AS (SELECT oldSecs.day         d1,
+                                                   newEntry.day        d2,
+                                                   oldSecs.start_time  s1,
+                                                   newEntry.start_time s2,
+                                                   oldSecs.end_time    e1,
+                                                   newEntry.end_time   e2
+                                            FROM oldSecs
+                                                     INNER JOIN newEntry ON oldSecs.day = newEntry.day
+                                            WHERE ((oldSecs.start_time >= newEntry.start_time) AND
+                                                   (oldSecs.start_time < newEntry.end_time))
+                                               OR ((newEntry.start_time >= oldSecs.start_time) AND
+                                                   (newEntry.start_time < oldSecs.end_time))
+    );
 
-        SELECT count(*) INTO @confCount FROM conflictSecs;
+    SELECT count(*) INTO @confCount FROM conflictSecs;
 
-        IF (@confCount > 0) THEN
-            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT =
-                    'The room is already reserved for another section at this time';
-        END IF;
+    IF (@confCount > 0) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT =
+                'The room is already reserved for another section at this time';
+    END IF;
 
     /******************* Instructor Time Conflict Check *******************/
     IF NEW.type = 'lecture' THEN
