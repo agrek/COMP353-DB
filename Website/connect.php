ea088ca3f0e0a61012337e2f4b5cc14bbafb8b96
osamaqalam <osamaqalam@users.noreply.github.com>
2019-08-12 11:25:08 -0400

    removed comment

--- Website/connect.php
+++ Website/connect.php
@@ -10,19 +10,5 @@
 	{
 	  die('Could not connect: ' . mysql_error());
 	}
-/*
-	$query = 'select * from Students1';
-	//Get Result
-  $result = mysqli_query($con, $query);
 
-  if (!$result) {
-      printf("Error: %s\n", mysqli_error($con));
-      exit();
-  }
-
-	//Fetch Data
-	$posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-	var_dump($posts);
-*/
 ?>
